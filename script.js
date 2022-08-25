183a6119dff8afcb38790c4743c0bf50f69b6e64
AnuragParm <111700299+AnuragParm@users.noreply.github.com>
2022-08-25 09:35:39 +0100

    Cart Update count

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const productElement = document.querySelector(".productRender");
 const cartElement = document.querySelector(".cartItems");
+const subTotalElement = document.querySelector(".subTotal");
 
 //Render Product Items
 function renderProducts() {
@@ -30,16 +31,37 @@ renderProducts();
 let cart = [];
 //Add To Cart
 function addToCart(id) {
-  const item = products.find((product) => product.id === id);
-  cart.push({ ...item, numberOfUnits: 1 });
-  // console.log(cart);
+  //Check if product already exist in cart
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else {
+    const item = products.find((product) => product.id === id);
+    cart.push({ ...item, numberOfUnits: 1 });
+    // console.log(cart);
+  }
+  updateCart();
+}
 
+//change number of units in an item
+function changeNumberOfUnits(action, id) {
+  cart = cart.map((item) => {
+    let oldNumberOfUnits = item.numberOfUnits;
+    if (item.id === id) {
+      if (action === "minus" && oldNumberOfUnits > 1) {
+        oldNumberOfUnits--;
+      } else if (action === "plus" && oldNumberOfUnits < item.instock) {
+        oldNumberOfUnits++;
+      }
+    }
+    return { ...item, numberOfUnits: oldNumberOfUnits };
+  });
   updateCart();
 }
 
 //Update Cart
 function updateCart(params) {
   renderCartItems();
+  renderSubtotal();
 }
 
 //render Cart Items
@@ -54,8 +76,25 @@ function renderCartItems() {
                   <h6>Item: ${item.name}</h6>
                 </div>
               </div>
-              <span> 1 </span>
+              <span><button class="btn btn-danger" onClick="changeNumberOfUnits('minus', ${item.id})"><i class="fa-solid fa-minus"></i></button>
+              ${item.numberOfUnits} 
+              <button class="btn btn-success" onClick="changeNumberOfUnits('plus', ${item.id})"><i class="fa-solid fa-plus"></i></button>
+              </span>
               <span>Sub-Total: £ ${item.price}</span>
             </div>        `;
   });
+}
+//Calculate Cart Subtotal
+function renderSubtotal() {
+  let totalPrice = 0;
+  let totalItem = 0;
+
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
+    totalItem += item.numberOfUnits;
+  });
+  subTotalElement.innerHTML = `
+  total (${totalItem} items): £ ${totalPrice.toFixed(2)}
+  `;
+  totalItemCart.innerHTML = totalItem;
 }
