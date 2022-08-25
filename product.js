7c729eefdfb6814a290c8160c696b0e31df41f23
AnuragParm <111700299+AnuragParm@users.noreply.github.com>
2022-08-25 08:17:08 +0100

    First Commit: HTML Static page

---
+++ product.js
@@ -0,0 +1,35 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+const products = [
+  {
+    id: 0,
+    name: "A",
+    description: " 3 for 1.30",
+    img: "apple.jpeg",
+    price: 0.5,
+    instock: 25,
+  },
+  {
+    id: 1,
+    name: "B",
+    description: " 2 for 0.45",
+    img: "banana.jpeg",
+    price: 0.3,
+    instock: 25,
+  },
+  {
+    id: 2,
+    name: "C",
+    description: " 5 for 0.80",
+    img: "carrot.jpeg",
+    price: 0.2,
+    instock: 25,
+  },
+  {
+    id: 3,
+    name: "D",
+    description: " 5 for 0.50",
+    img: "drumstick.jpeg",
+    price: 0.15,
+    instock: 25,
+  },
+];
